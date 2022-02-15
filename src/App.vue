23f68f86cb3712b25328e041b815469892f4a6f2
bearholmes <bearholmes@gmail.com>
2022-02-15 13:02:40 +0900

    주석추가/ 코드정리

--- src/App.vue
+++ src/App.vue
@@ -18,15 +18,15 @@
             <th>음력</th>
             <th>한국나이</th>
 <!--            <th>만 나이</th>-->
-            <th>음력 > 양력</th>
+            <th>양력</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(item, idx) in list" :key="idx">
-            <td>{{item.dateSun}}</td>
+            <td>{{item.dateLunar}}</td>
             <td>{{item.korAge}}</td>
 <!--            <td>{{item.korAge - 1}}</td>-->
-            <td>{{item.date}}</td>
+            <td>{{item.dateSolar}}</td>
           </tr>
         </tbody>
       </table>
@@ -60,29 +60,35 @@ export default {
     onclickSubmit() {
       console.log('onclickSubmit')
       this.list = [];
+
       this.lunar = {
-        day: null,
-        dayOfWeek: null,
-        leapMonth: false,
-        month: null,
-        year: null,
+        day: null, // 일
+        dayOfWeek: null, // 요일
+        leapMonth: false, // 윤달=true/평달
+        month: null, // 월
+        year: null, // 연도
       };
+
+      // 입력한 날짜
       const { year, month, day } = this.form
+
+      // 입력한 날짜를 음력으로 변환
       this.lunar = getLunar(year, month, day)
-      const lunarMonth = this.lunar.month;
-      const lunarDay = this.lunar.day;
-      const lunarLeapMonth = this.lunar.leapMonth;
+      const lunarMonth = this.lunar.month; // 음력 생일 월
+      const lunarDay = this.lunar.day; // 음력 생일 일
+      const lunarLeapMonth = this.lunar.leapMonth; // 윤달여부
 
-
+      // 100세까지 배열에 추가
       for(let i = 0;i < 100; i++) {
-        const newYear = this.lunar.year + i;
+        const newYear = this.lunar.year + i; // 연도 증가
 
+        // 변환된 음력값(해당연도-음력월-음력일)을 통해 양력으로 변환
         const solar = getSolar(newYear, lunarMonth, lunarDay, lunarLeapMonth)
         this.list.push({
-          korAge: i + 1,
-          since: newYear,
-          dateSun: `${newYear}년 ${lunarMonth}월 ${lunarDay}일`,
-          date: `${solar.year}년 ${solar.month}월 ${solar.day}일 (${solar.dayOfWeek})`
+          korAge: i + 1, // 한국나이
+          year: newYear, // 연도
+          dateLunar: `${newYear}년 ${lunarMonth}월 ${lunarDay}일`, // 음력
+          dateSolar: `${solar.year}년 ${solar.month}월 ${solar.day}일 (${solar.dayOfWeek})` // 양력
         })
       }
 
